2b7a9359f2ebf7590d78baa15e96f24440d38211
Michael Kornelakis <michalis@cyberlogic.gr>
2022-05-20 12:59:21 +0300

    fix/imports in UserForm

--- components/UserForm/UserForm.tsx
+++ components/UserForm/UserForm.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from "react";
 import { isStyledComponent } from "styled-components";
 
 import { useForm } from "hooks/useForm";
-import { SFormWrapper, SForm } from "components/UserForm/styled";
+import { SUserFormWrapper, SUserForm } from "components/UserForm/styled";
 import { ColorSpan } from "components/ColorSpan";
 import { SSpecialButton } from "components/Header/styled";
 
@@ -25,8 +25,8 @@ export const UserForm = <T extends Record<string, unknown>>(
   );
 
   return (
-    <SFormWrapper>
-      <SForm>
+    <SUserFormWrapper>
+      <SUserForm>
         {React.Children.map(props.children, (child) => {
           if (React.isValidElement(child)) {
             const isInput =
@@ -57,7 +57,7 @@ export const UserForm = <T extends Record<string, unknown>>(
         <SSpecialButton type='submit' onClick={(e) => handleSubmit(e)}>
           {props.submitButtonTitle}
         </SSpecialButton>
-      </SForm>
-    </SFormWrapper>
+      </SUserForm>
+    </SUserFormWrapper>
   );
 };
