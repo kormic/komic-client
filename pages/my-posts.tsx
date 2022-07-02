4add26e93e82fc0e0a17ff24666f5b6ebffab246
Michael Kornelakis <michalis@cyberlogic.gr>
2022-07-03 00:40:24 +0300

    feat/improve my posts css

--- pages/my-posts.tsx
+++ pages/my-posts.tsx
@@ -9,6 +9,28 @@ import { MainTitle } from "components/MainTitle";
 import { SSpecialButton } from "components/Header/styled";
 import { getUserIdFromToken } from "shared/utils";
 import { EmptyPostsList } from "components/EmptyPostsList";
+import styled from "styled-components";
+
+const SWriteAPostWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  padding-bottom: 1em;
+
+  @media (min-width: 1024px) {
+    width: 50%;
+  }
+`;
+
+const SMyPostsWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  @media (min-width: 1024px) {
+    width: 50%;
+    padding: 0;
+  }
+`;
 
 const MyPosts = () => {
   const [posts, setPosts] = useState<PostDTO[]>([]);
@@ -29,19 +51,12 @@ const MyPosts = () => {
   return (
     <Layout>
       <MainTitle title='Your Posts' />
-      <div
-        style={{
-          width: "50%",
-          margin: "auto",
-          display: "flex",
-          paddingBottom: "1em",
-        }}
-      >
+      <SWriteAPostWrapper>
         <SSpecialButton type='button' onClick={() => router.push("/new-post")}>
           Write a Post
         </SSpecialButton>
-      </div>
-      <div style={{ width: "50%", margin: "auto" }}>
+      </SWriteAPostWrapper>
+      <SMyPostsWrapper>
         {posts.length > 0 ? (
           posts.map((post) => (
             <PostPreview key={post.id} post={post} showTags={false} />
@@ -52,7 +67,7 @@ const MyPosts = () => {
             text="You haven't posted anything yet."
           />
         )}
-      </div>
+      </SMyPostsWrapper>
     </Layout>
   );
 };
